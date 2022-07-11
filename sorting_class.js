be63c903843395cac28e83f0c8934eafe6f391ed
Julien Armagnac <julien.tamil@gmail.com>
2022-07-11 17:17:28 +0200

    Add Insert Sort (but not recursive yet).

--- sorting_class.js
+++ sorting_class.js
@@ -1,6 +1,43 @@
 const fs = require('fs');
 const prompt = require("prompt-sync")({ sigint: true });
 
+// Les 3 fonctions qui suivent valident et formattent les données du fichier list.txt en un tableau
+function validateInputFile(argument) {
+    if (argument === "list.txt") {
+        return true;
+    }
+    else {
+        console.log("Erreur : le nom du fichier de données doit être list.txt.");
+        return false;
+    }
+}
+
+function getDataFromFile(fileName) {
+    var data;
+    try {
+        data = fs.readFileSync(fileName, 'utf8');
+    } catch (error) {
+        console.error(error.message);
+    }
+    return data;
+}
+
+function dataToArray(data) {
+    let array = data.split(' ');
+    array = array.map(string => string = parseInt(string));
+    return array;
+}
+// Fin des 3 fonctions de validation et formatage des données de list.txt en un tableau
+
+// La fonction suivante ré-intialise le tableau de données à trier.
+function dataReset(){
+    if (validateInputFile(process.argv[2])) {
+        const data = getDataFromFile(process.argv[2]);
+        const dataArray = dataToArray(data);
+        return dataArray;
+    }
+}
+
 class Sort {
     constructor(arrayToSort) {
         this.arrayToSort = arrayToSort;
@@ -27,45 +64,43 @@ class Sort {
         
         this.bubbleSort(array, limit - 1);
     }
-}
 
-
-function validateInputFile(argument) {
-    if (argument === "list.txt") {
-        return true;
+    insertionSort(array) {
+        for(let i = 1 ; i < array.length ; i++) {
+            let current = array[i];
+            let j = i -1;
+            while(j >= 0 && array[j] > current) {
+                this.nbComparisons++;
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
     }
-    else {
-        console.log("Erreur : le nom du fichier de données doit être list.txt.");
-        return false;
-    }
-}
-
-function getDataFromFile(fileName) {
-    var data;
-    try {
-        data = fs.readFileSync(fileName, 'utf8');
-    } catch (error) {
-        console.error(error.message);
-    }
-    return data;
-}
-
-function dataToArray(data) {
-    let array = data.split(' ');
-    array = array.map(string => string = parseInt(string));
-    return array;
 }
 
 function perform () {
-    if (validateInputFile(process.argv[2])) {
-        data = getDataFromFile(process.argv[2]);
-        dataArray = dataToArray(data);
-    }
 
     // Lancement du tri à bulles de list.txt
-    let sort1 = new Sort(dataArray);
-    sort1.bubbleSort(dataArray, dataArray.length - 1);
-    console.log (`Tri à bulles : ${sort1.nbComparisons} comparaisons - [${sort1.arrayToSort}].`);
+    var rawArray = dataReset();
+        
+    let sort1 = new Sort(rawArray);
+    sort1.bubbleSort(rawArray, rawArray.length - 1);
+    console.log(`Tri à bulles : ${sort1.nbComparisons} comparaisons - [${sort1.arrayToSort}].`);
+    prompt("[ENTER]");
+
+    // Lancement du tri par insertion de list.txt
+    rawArray = dataReset();
+  
+    let sort2 = new Sort(rawArray);
+    sort2.insertionSort(rawArray,0);
+    console.log(`Tri par insertion : ${sort2.nbComparisons} comparaisons - [${sort2.arrayToSort}].`);
+    prompt("[ENTER]");
+
+    // Lancement du tri par sélection de list.txt
+    rawArray = dataReset();
+  
+    let sort3 = new Sort(rawArray);
 }
 
 perform();
